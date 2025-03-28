342b34c8b2e56e5bdf1bbe3a5a00bac4062d239e
greenboy106 <164964696+greenboy106@users.noreply.github.com>
2025-03-28 18:21:54 +1100

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,7 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Mesh_Floor"
+#define SECRET_PASS "thaochi0299"
+#define SECRET_CH_ID 2896684            // Replace with your ThingSpeak Channel Number
+#define SECRET_WRITE_APIKEY "B20DC08PAT8LDNNM"  // Replace with your ThingSpeak Write API Key
